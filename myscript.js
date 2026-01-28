4dcce1cab747b5c5a9ac79525521d54968b0385e
Sara-Fioretti <fiorettisara22@gmail.com>
2026-01-28 11:21:28 +0100

    Inizializzazione delle variabili con il prompt

---
+++ myscript.js
@@ -0,0 +1,8 @@
+/* PROCEDIMENTO
+1)Stampo a video la successione di operazioni "Inserire chilometri da percorrere ed età passeggero per calcolare il prezzo totale del viaggio"
+2)Inizializzo la prima variabile con il prompt (Inserire chilometri da percorrere)
+3)Inizializzo la seconda variabile con il prompt (Inserire l'età del passeggero)
+4)Imposto un if-elseif-else e stampo a video il risultato
+*/
+
+console.log("Inserire chilometri da percorrere ed età passeggero per calcolare il prezzo totale del viaggio")
